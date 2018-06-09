32b44813d73bc87f2f93676dee108711c4fd6e3d
doubleface <christophe@cozycloud.cc>
2018-06-10 00:45:42 +0200

    fix: some more packaging fixes for better travis publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
+const entry = require('./package.json').main
 
 module.exports = {
-  entry: './src/index.ts',
+  entry,
   target: 'node',
   mode: 'none',
   module: {
@@ -27,6 +28,7 @@ module.exports = {
       { from: 'package.json' },
       { from: 'README.md' },
       { from: 'assets' },
+      { from: '.travis.yml' },
       { from: 'LICENSE' },
       { from: 'tor-logo.jpg' }
     ])
